4247631a501ec97e6f88649113c89e7230c9c1d4
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-03-02 14:53:01 +0100

    test(linked-list): introduce toVector and buildList utilities for building and validating list contents

---
+++ unit_tests/test_utils.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <initializer_list>
+#include <vector>
+
+#include "api.hpp"
+
+// Convert Node chain to std::vector<int> for easy comparisons in tests.
+inline std::vector<int> toVector(const Node* head) {
+    std::vector<int> out;
+    const Node* cur = head;
+    while (cur != nullptr) {
+        out.push_back(cur->m_val);
+        cur = cur->next;
+    }
+    return out;
+}
+
+// Build a list quickly in tests without manual append loops.
+// Example: buildList(ll, {3,5,5,6});
+inline void buildList(LinkedList& ll, std::initializer_list<int> values) {
+    for (int x : values) {
+        ll.append(x);
+    }
+}
